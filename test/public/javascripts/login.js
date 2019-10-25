e31d56d18056eddaccd02bf55dfcd876fa2af5d1
chensiyuan <chensiyuan0214@hotmail.com>
2019-10-25 17:15:27 -0400

    update

--- test/public/javascripts/login.js
+++ test/public/javascripts/login.js
@@ -1,32 +1,56 @@
 'use strict'
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/my_test', {userNewUrlParser: true});
+var db = mongoose.connection;
+
+
+
+window.localStorage.clear();
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
-var loginButton = document.getElementById("loginButton");
-var users = [];
+let user =new Array();
+window.localStorage.setItem('textValues', JSON.stringify(user));
+var Person = function (username, password) {
+  this.username = username;
+  this.password = password;
 
+  this.toString = function () {
+    return ["Hi ! I'm ", this.username, " ", this.password].join("");
+  };
+};
 function register() {
   var username = document.getElementById("exampleInputEmail2").value;
   var password = document.getElementById("exampleInputPassword2").value;
-  var person = {username: username, password: password};
-
-  for(let i = 0; i < users.length; i++) {
-    if(users[i].username == username){
+  var p = new Person(username, password);
+  alert(p);
+  user = window.localStorage.getItem('textValues');
+  alert(window.localStorage.getItem('textValues'));
+  alert(user);
+  alert("最起码进来了");
+  for(let i = 2; i < user.length; i++) {
+    alert(user[i].username);
+    if(user[i].username == username){
         alert("Username is already existed");
         return false;
     }
   }
-  users.push(person);
+  user.push(p);
+  alert("users"+user);
+  window.localStorage.setItem('textValues', JSON.stringify(user));
+  alert(user.length);
 
 }
 
 function validate(){
   var username = document.getElementById("exampleInputEmail1").value;
   var password = document.getElementById("exampleInputPassword1").value;
+  alert(users.length);
   for(let i = 0; i < users.length; i++) {
-
       if ( users[i].username == username && password == users[i].password){
           alert ("Login successfully");
-          window.location.replace("http://www.w3schools.com");
+          var loginLabel = document.getElementById("loginLabel");
+          loginLabel.innerText = "Welcome";
+          // window.location.replace("http://www.w3schools.com");
           return false;
       }
 
